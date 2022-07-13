b4bb35b96bf556c2bbda06433f242ac6f3fd7a1c
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-13 16:09:49 -0400

    Create Home Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './Views/Home';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
+      <Footer />
     </BrowserRouter>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import * as bootstrap from 'bootstrap';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
